a3cd7dc66e836de838f0552e43f4e41cde843783
Константин Степнин <StepninKV@it.mos.ru>
2020-04-29 18:10:30 +0300

    Добавлен web-socket пример

---
+++ web-socket/src/main/java/com/example/web/socket/domain/Greeting.java
@@ -0,0 +1,12 @@
+package com.example.web.socket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Greeting {
+
+    private String content;
+
+}
---
+++ web-socket/src/main/java/com/example/web/socket/domain/HelloMessage.java
@@ -0,0 +1,10 @@
+package com.example.web.socket.domain;
+
+import lombok.Data;
+
+@Data
+public class HelloMessage {
+
+    private String name;
+
+}
